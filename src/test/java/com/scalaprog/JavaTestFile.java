bf335fd613b087a0387f19da8ed2dbdca8c60983
Soren Mathiasen <soren@schantz.com>
2013-04-23 22:13:47 +0200

    misc fixes

--- src/test/java/com/scalaprog/JavaTestFile.java
+++ src/test/java/com/scalaprog/JavaTestFile.java
@@ -1,25 +1,26 @@
 package com.scalaprog;
 /**
- * User: soren
- */
-/**
  * @documentation
  *
- * # This is simple piece of documentation
+ * # Foxy Doxy
  *
- * This is so more cool than normal java doc.
+ * This is a library for generating documentation from the code base.
  *
- * @section Configuration
+ * How does this differ from JavaDoc ?
+ *
+ * JavaDoc was designed to document Java APIs, and is good at that. But in order for the user to find the documentation,
+ * you need to know what class to look for.
+ *
+ * Sometimes you need an other kind of documentation. Let's say you would describe the flow of the application, the bootstrapping process or some other kind
+ * that spans multiple classes and concepts.
+ *
+ * But at the same time you need it close to the source code, in order for it to be kept up to date.
+ *
+ * __Then Foxy Doxy is the tool for you.__
+ *
+ * @section Sample
  * @tags test, demo, simple
  */
 public class JavaTestFile {
 
-
-    /**
-     * this is not
-     * @param name
-     */
-    public void test(String name) {
-
-    }
 }
